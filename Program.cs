6131e038355078e0c9bdd0d937c66a41486d7b7d
Ego  git config --global user.email <you@example.com>
2023-01-28 13:09:18 +0300

    Tusk_3

--- Program.cs
+++ Program.cs
@@ -66,3 +66,24 @@ else
     System.Console.WriteLine(Last_Number2);
 }
 
+
+/* Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+4 -> да
+-3 -> нет
+7 -> нет
+*/
+
+System.Console.WriteLine("_____Задача 6_____");
+
+System.Console.Write("Enter number:");
+
+int Number = Convert.ToInt32(Console.ReadLine());
+
+if (Number % 2 == 0)
+{
+    System.Console.WriteLine("Четное!!!");
+}
+else 
+{
+    System.Console.WriteLine("Не четное!!!");
+}
